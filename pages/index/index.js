893f23afceedee33e32caf40fc19a77f4a809d8e
qiminghao <qiminghao@heyhomi.com>
2019-01-09 14:54:19 +0800

    代码更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,42 @@ var base64 = require("../../images/base64");
 
 Page({
   data: {
-    name: 'qmh',
-    userInfo: {},
+    list: [{
+      'name': '张京京',
+      'desc': '我是一个大吃货',
+      'text': '我今天吃了好多好吃的,好开心噢噢噢噢噢噢噢噢哦哦哦哦哦哦哦哦哦',
+       'ifImg':true
+    }, {
+      'name': '齐明浩',
+      'desc': '我真滴不爱吃饭',
+      'text': '我真滴不喜欢吃饭嗷铁汁们',
+      'ifImg': false
+      }, {
+        'name': '齐明浩',
+        'desc': '我真滴不爱吃饭',
+        'text': '我真滴不喜欢吃饭嗷铁汁们',
+        'ifImg': false
+      }, {
+        'name': '齐明浩',
+        'desc': '我真滴不爱吃饭',
+        'text': '我真滴不喜欢吃饭嗷铁汁们',
+        'ifImg': false
+      }, {
+        'name': '齐明浩',
+        'desc': '我真滴不爱吃饭',
+        'text': '我真滴不喜欢吃饭嗷铁汁们',
+        'ifImg': false
+      }, {
+        'name': '齐明浩',
+        'desc': '我真滴不爱吃饭',
+        'text': '我真滴不喜欢吃饭嗷铁汁们',
+        'ifImg': false
+      }, {
+        'name': '齐明浩',
+        'desc': '我真滴不爱吃饭',
+        'text': '我真滴不喜欢吃饭嗷铁汁们',
+        'ifImg': false
+      }],
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -16,7 +50,7 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     this.setData({
       icon20: base64.icon20,
       icon60: base64.icon60
